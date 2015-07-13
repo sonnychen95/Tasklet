477e234fb6b5dfdcce003a287d9d8fa6ecbd07fe
Sonny Chen <sonny.chen@berkeley.edu>
2015-07-13 12:54:22 -0400

    clear button, fix bugs

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
 
-	var maxappend = 0;
+	var maxappend = $("div.item").length;
 
 
 	$("#add").on("touchstart", function() {
-		if (maxappend < 10) {
-		$("#list").append("<div id='item'><textarea cols='29' rows='2' maxlength='80' placeholder='Task' style='background:none; border:medium none;''></textarea></div>");
-		maxappend++
+		if (maxappend < 11) {
+		$("#list").append("<div class='item'><textarea cols='29' rows='2' maxlength='80' placeholder='Task' style='background:none; border:medium none;''></textarea></div>");
+		$("#delete").css("display", "block");
 		} 
 	});
 
-	$(document).on("swipe", "#item", function() {
+
+	$("#delete").on("touchstart", function() {
+		
+			$(".done").remove();
+	
+	});
+
+	$(document).on("swipe", ".item", function() {
 		if($(this).css("height")=="68px") {
-    	$(this).css("background-color", "#8f9fbc");
- 		$(this).css("color", "red");
- 		$(this).css("height", "69px");
- 		$("textarea").prop("disabled", true);
+    	$(this).css("background-color", "#3e4554");
+    	$(this).css("height", "69px");
+    	$(this).addClass("done");
+
  		} else {
  			$(this).css("background-color", "white");
  			$(this).css("height", "68px");
  			$("textarea").prop("disabled", false);
-
+ 			$(this).removeClass("done");
+ 			$("textarea").css("color", "#636875");
  		}
 	});
+
+	$(document).on("swipe", "textarea", function() {
+		if($(".item").css("height")=="68px"); {
+ 		$(this).css("color", "#636875");
+ 		$(this).prop("disabled", true);
+		}  
+	});
+
+
 
     
 });
