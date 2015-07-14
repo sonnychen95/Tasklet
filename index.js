a8ad06a58bf29fea525180d92645e48b817f46bd
Sonny Chen <sonny.chen@berkeley.edu>
2015-07-14 16:52:06 -0400

    config, change font, and colors

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
 
-	var maxappend = $("div.item").length;
+	var maxappend = $(".item").length;
+	var d = new Date();
+	var mm = d.getMonth()+1;
+	var dd = d.getDate();
+	var today = mm+"/"+dd;
+
+	$("#date").text(today);
 
 
 	$("#add").on("touchstart", function() {
-		if (maxappend < 11) {
-		$("#list").append("<div class='item'><textarea cols='29' rows='2' maxlength='80' placeholder='Task' style='background:none; border:medium none;''></textarea></div>");
+		if (maxappend < 3) {
+		$("#list").append("<div class='item'><textarea cols='29' rows='1' maxlength='40' placeholder='Task' style='background:none; border:medium none;''></textarea></div>");
 		$("#delete").css("display", "block");
-		} 
+		alert(maxappend);
+		}
 	});
 
-
-	$("#delete").on("touchstart", function() {
-		
-			$(".done").remove();
 	
-	});
+	$("#delete").on("touchstart", function() {
+		$(".done").remove();
+	});		
 
 	$(document).on("swipe", ".item", function() {
-		if($(this).css("height")=="68px") {
-    	$(this).css("background-color", "#3e4554");
-    	$(this).css("height", "69px");
+		if($(this).css("height")=="75px") {
+    	$(this).css("background-color", "#40586b");
+    	$(this).css("height", "76px");
     	$(this).addClass("done");
 
  		} else {
  			$(this).css("background-color", "white");
- 			$(this).css("height", "68px");
+ 			$(this).css("height", "75px");
  			$("textarea").prop("disabled", false);
  			$(this).removeClass("done");
  			$("textarea").css("color", "#636875");
@@ -33,8 +38,8 @@ $(document).ready(function(){
 	});
 
 	$(document).on("swipe", "textarea", function() {
-		if($(".item").css("height")=="68px"); {
- 		$(this).css("color", "#636875");
+		if($(".item").css("height")=="75px"); {
+ 		$(this).css("color", "#fff");
  		$(this).prop("disabled", true);
 		}  
 	});
